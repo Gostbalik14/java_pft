bf49c8be97b57de58f56d17fd2e72e4d870028a7
Gostbalik14 <108429512+Gostbalik14@users.noreply.github.com>
2022-07-04 17:44:19 +0300

    Изменены настройки форматирования

--- sandbox/src/main/java/ua/stqa/pft/sandbox/HelloWorld.java
+++ sandbox/src/main/java/ua/stqa/pft/sandbox/HelloWorld.java
@@ -1,8 +1,8 @@
 package ua.stqa.pft.sandbox;
 
 class HelloWorld {
-    public static void main(String[] args) {
-        // Display "Hello World!"
-        System.out.println("Hello World!");
-    }
+  public static void main(String[] args) {
+    // Display "Hello World!"
+    System.out.println("Hello World!");
+  }
 }
